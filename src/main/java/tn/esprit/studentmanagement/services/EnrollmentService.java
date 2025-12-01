162c9a2c0164d451f59c1fca6da5bfab76068e70
oussema ben el haj <126527296+oussemabb20@users.noreply.github.com>
2025-12-01 22:15:10 +0100

    fix sonar qube

--- src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
@@ -1,6 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.repositories.EnrollmentRepository;
@@ -19,7 +18,7 @@ public class EnrollmentService implements IEnrollment {
 
     @Override
     public Enrollment getEnrollmentById(Long idEnrollment) {
-        return enrollmentRepository.findById(idEnrollment).get();
+        return enrollmentRepository.findById(idEnrollment).orElse(null);
     }
 
     @Override
