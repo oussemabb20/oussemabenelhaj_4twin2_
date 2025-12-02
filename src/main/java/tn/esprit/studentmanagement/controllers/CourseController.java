2a4de661c5490504867d9860dc496670dea8cd37
oussema ben el haj <126527296+oussemabb20@users.noreply.github.com>
2025-12-02 10:07:15 +0100

    fix

---
+++ src/main/java/tn/esprit/studentmanagement/controllers/CourseController.java
@@ -0,0 +1,41 @@
+package tn.esprit.studentmanagement.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.studentmanagement.entities.Course;
+import tn.esprit.studentmanagement.services.ICourseService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Course")
+@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+public class CourseController {
+    private ICourseService courseService;
+
+    @GetMapping("/getAllCourse")
+    public List<Course> getAllCourse() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/getCourse/{id}")
+    public Course getCourse(@PathVariable Long id) {
+        return courseService.getCourseById(id);
+    }
+
+    @PostMapping("/createCourse")
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+
+    @PutMapping("/updateCourse")
+    public Course updateCourse(@RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+
+    @DeleteMapping("/deleteCourse/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseService.deleteCourse(id);
+    }
+}
