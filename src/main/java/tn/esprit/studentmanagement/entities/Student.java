fe2ae4c6f86ce45044aed7fb9036ff20e4bfe064
oussema ben el haj <126527296+oussemabb20@users.noreply.github.com>
2025-12-15 23:25:44 +0100

    fix

--- src/main/java/tn/esprit/studentmanagement/entities/Student.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Student.java
@@ -1,6 +1,7 @@
 package tn.esprit.studentmanagement.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -25,6 +26,7 @@ public class Student {
     private String address;
 
     @ManyToOne
+    @JsonIgnoreProperties("students")
     private Department department;
 
     @OneToMany(mappedBy = "student")
